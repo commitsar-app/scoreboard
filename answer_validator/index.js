e653358e1c2c88326546f588f14e3a70420f65bd
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2023-12-21 04:24:28 +0900

    Add index.js

---
+++ answer_validator/index.js
@@ -0,0 +1,6 @@
+const CLIENT_PAYLOAD = process.env.CLIENT_PAYLOAD;
+if (!CLIENT_PAYLOAD) {
+  throw new Error("UNEXPECTED: CLIENT_PAYLOAD environment variable is not set");
+}
+
+console.log(JSON.parse(CLIENT_PAYLOAD));
