7de99c9317ec3f2966ee3d4b70f315acb23fb6ea
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2023-12-21 05:18:55 +0900

    Modify env var name

--- answer_validator/index.js
+++ answer_validator/index.js
@@ -1,4 +1,4 @@
-const CLIENT_PAYLOAD = process.env.CLIENT_PAYLOAD;
+const CLIENT_PAYLOAD = process.env.ENCRYPTED_CLIENT_PAYLOAD;
 if (!CLIENT_PAYLOAD) {
   throw new Error("UNEXPECTED: CLIENT_PAYLOAD environment variable is not set");
 }
