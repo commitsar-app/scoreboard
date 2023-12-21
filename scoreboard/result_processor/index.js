f6730fb327954c2bad6233f1ed0c6c67d124872a
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2023-12-21 14:01:46 +0900

    Fix the issue body

--- scoreboard/result_processor/index.js
+++ scoreboard/result_processor/index.js
@@ -12,7 +12,7 @@ const result = JSON.parse(fs.readFileSync("./result.json", "utf8"));
 
     } else if (conclusion === "failure") {
         const message = template.answerFailedTemplate(result.sender, result.challengeId, result.answerUrl, result.error);
-        const resp = await fetch("https://api.github.com/repos/ryotak-ctf/scoreboard/issues", {
+        let resp = await fetch("https://api.github.com/repos/ryotak-ctf/scoreboard/issues", {
             method: "POST",
             headers: {
                 Authorization: `token ${token}`,
@@ -27,6 +27,23 @@ const result = JSON.parse(fs.readFileSync("./result.json", "utf8"));
         if (!resp.ok) {
             throw new Error(`Failed to create an issue: ${resp.status} ${resp.statusText}`);
         }
+        const issue = await resp.json();
+        // close the issue
+        resp = await fetch(`https://api.github.com/repos/ryotak-ctf/scoreboard/issues/${issue.number}`, {
+            method: "PATCH",
+            headers: {
+                Authorization: `token ${token}`,
+                "Content-Type": "application/json",
+                "User-Agent": "ryotak-ctf scoreboard"
+            },
+            body: JSON.stringify({
+                state: "closed"
+            })
+        });
+        if (!resp.ok) {
+            throw new Error(`Failed to close the issue: ${resp.status} ${resp.statusText}`);
+        }
+
     } else {
         throw new Error(`UNEXPECTED: conclusion is not success or failure: ${conclusion}`);
     }
--- scoreboard/result_processor/template.js
+++ scoreboard/result_processor/template.js
@@ -1,13 +1,13 @@
 function answerFailedTemplate (sender, challengeId, answerUrl, error) {
     return `
-    Hello @${sender}! [Your answer](${answerUrl}) for challenge ${challengeId} failed to pass the validation.
-    Please check the error message below for more details:
-    \`\`\`
-    ${error}
-    \`\`\`
+Hello @${sender}! [Your answer](${answerUrl}) for challenge ${challengeId} failed to pass the validation.
+Please check the error message below for more details:
+\`\`\`
+${error}
+\`\`\`
 
-    If you believe this is an error, please contact [RyotaK](https://twitter.com/ryotkak) or [open an issue](https://github.com/ryotak-ctf/scoreboard/issues/new).
-    `;
+If you believe this is an error, please contact [RyotaK](https://twitter.com/ryotkak) or [open an issue](https://github.com/ryotak-ctf/scoreboard/issues/new).
+`;
 }
 
 exports.answerFailedTemplate = answerFailedTemplate;
