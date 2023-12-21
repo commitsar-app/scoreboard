00beddb40836eca44b3f69b8bb6a46916a69901d
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2023-12-21 17:53:09 +0900

    Scoreboard updater

--- scoreboard/result_processor/index.js
+++ scoreboard/result_processor/index.js
@@ -12,6 +12,24 @@ const result = JSON.parse(fs.readFileSync("./result.json", "utf8"));
     if (result.correct) {
         title = `Answer for challenge ${result.challengeId} is correct`;
         message = template.answerCorrectTemplate(result.sender, result.challengeId, result.answerUrl);
+        const resp = await fetch("https://api.github.com/repos/ryotak-ctf/scoreboard/dispatches", {
+            method: "POST",
+            headers: {
+                Authorization: `token ${token}`,
+                "Content-Type": "application/json",
+                "User-Agent": "ryotak-ctf scoreboard"
+            },
+            body: JSON.stringify({
+                event_type: "update_scoreboard",
+                client_payload: {
+                    sender: result.sender,
+                    challengeId: result.challengeId,
+                }
+            })
+        });
+        if (!resp.ok) {
+            throw new Error(`Failed to dispatch an event: ${resp.status} ${resp.statusText}`);
+        }
     } else {
         title = `Answer for challenge ${result.challengeId} failed`;
         message = template.answerFailedTemplate(result.sender, result.challengeId, result.answerUrl, result.error);
---
+++ scoreboard/scoreboard_updater/index.js
@@ -0,0 +1,2 @@
+console.log(process.env.CHALLENGE_ID);
+console.log(process.env.SENDER);
