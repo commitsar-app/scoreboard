86ddf5349fdfbc13d1102d6aed9ace87beafe23d
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2023-12-21 12:43:56 +0900

    Testing mask

--- scoreboard/answer_validator/index.js
+++ scoreboard/answer_validator/index.js
@@ -1,58 +1,4 @@
-const crypto = require('crypto');
-
-// took from https://gist.github.com/junderw/1d41158403978ba0363e5868d4f434d9
-async function decrypt(ciphertext, password, difficulty = 10) {
-	const ciphertextBuffer = Array.from(base64Decode(ciphertext))
-	const hashKey = await grindKey(password, difficulty)
-
-	const key = await crypto.subtle.importKey(
-		'raw',
-		hashKey, {
-		name: 'AES-GCM',
-	},
-		false,
-		['decrypt']
-	)
-
-	const decrypted = await crypto.subtle.decrypt({
-		name: 'AES-GCM',
-		iv: new Uint8Array(ciphertextBuffer.slice(0, 12)),
-		tagLength: 128,
-	},
-		key,
-		new Uint8Array(ciphertextBuffer.slice(12))
-	)
-
-	return new TextDecoder('utf-8').decode(new Uint8Array(decrypted))
-}
-
-function base64Decode(str) {
-	return new Uint8Array(atob(str).split('').map(c => c.charCodeAt(0)))
-}
-
-function grindKey(password, difficulty) {
-	return pbkdf2(password, password + password, Math.pow(2, difficulty), 32, 'SHA-256')
-}
-
-async function pbkdf2(message, salt, iterations, keyLen, algorithm) {
-	const msgBuffer = new TextEncoder().encode(message)
-	const msgUint8Array = new Uint8Array(msgBuffer)
-	const saltBuffer = new TextEncoder().encode(salt)
-	const saltUint8Array = new Uint8Array(saltBuffer)
-
-	const key = await crypto.subtle.importKey('raw', msgUint8Array, {
-		name: 'PBKDF2'
-	}, false, ['deriveBits'])
-
-	const buffer = await crypto.subtle.deriveBits({
-		name: 'PBKDF2',
-		salt: saltUint8Array,
-		iterations: iterations,
-		hash: algorithm
-	}, key, keyLen * 8)
-
-	return new Uint8Array(buffer)
-}
+const { decrypt } = require("./encryption.js");
 
 const CLIENT_PAYLOAD = process.env.ENCRYPTED_CLIENT_PAYLOAD;
 if (!CLIENT_PAYLOAD) {
@@ -68,30 +14,25 @@ if (!ENCRYPTION_PASSWORD) {
 	try {
 		eventPayload = JSON.parse(await decrypt(CLIENT_PAYLOAD, ENCRYPTION_PASSWORD));
 	} catch (e) {
-		console.error(`Could not decrypt payload: ${e}. Check your ENCRYPTION_PASSWORD.`);
-		process.exit(1);
+		throw new Error(`Could not decrypt payload: ${e}. Check your ENCRYPTION_PASSWORD.`);
 	}
 
 	if(!eventPayload) {
-		console.error("Payload could not be parsed.");
-		process.exit(1);
+		throw new Error("Payload could not be parsed.");
 	}
 
 	const advisory = eventPayload.repository_advisory;
 	if(!advisory) {
-		console.error("No advisory found in payload.");
-		process.exit(1);
+		throw new Error("No advisory found in payload.");
 	}
 	const problemId = advisory.summary?.trim();
 	if(!problemId) {
-		console.error("No problem ID found in advisory.");
-		process.exit(1);
+		throw new Error("No problem ID found in advisory.");
 	}
 	// check if the problem id is an integer
 	const problemIdNum = Number(problemId);
 	if(!Number.isInteger(problemIdNum)) {
-		console.error("Problem ID is not an integer.");
-		process.exit(1);
+		throw new Error("Problem ID is not an integer.");
 	}
 	const sender = eventPayload.sender?.login;
 	if(!sender) {
@@ -103,6 +44,10 @@ if (!ENCRYPTION_PASSWORD) {
 		console.error("No answer found in payload.");
 		process.exit(1);
 	}
+	
+
+	// mask the answer
+	console.log(`::add-mask::${answer}`)
 
 	console.log(`Problem ID: ${problemId}\nSender: ${sender}\nAnswer: ${answer}`);
 })();
